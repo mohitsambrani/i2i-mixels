445b4d6af35b3d2cb07422442bc40685d41f5091
mohitsambrani <mohitmas9@gmail.com>
2017-06-05 19:22:12 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,18 @@
 import cv2
+from operator import itemgetter
 img2=cv2.imread("1.png")
 img1=cv2.imread("1.png",0)
 
 _,img=cv2.threshold(img1,40,255,cv2.THRESH_BINARY)
-cv2.imshow("ADS",img)
 contours, hierarchy = cv2.findContours(img,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-cnt=contours[1]
-cv2.drawContours(img2, contours, 1, (0, 255, 0), 20)
-print contours
+area=[]
+for cnt in contours:
+    area.append(int(cv2.contourArea(cnt)))
+area.sort(reverse=True)
+for cnt in contours:
+    if(area[0]==int(cv2.contourArea(cnt))):
+        cnt1=cnt
+cnt=cnt1
 cv2.imshow("wr",img2)
 M=cv2.moments(cnt)
 cx=int(M["m10"]/M["m00"])
